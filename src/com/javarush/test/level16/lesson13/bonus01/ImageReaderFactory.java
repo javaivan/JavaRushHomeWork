e6a9b60e3d2e18ce5c3cbfd03adfdea8283148ba
ivan <ivanmix_88@mail.ru>
2016-10-24 21:10:04 +0300

    Level16

---
+++ src/com/javarush/test/level16/lesson13/bonus01/ImageReaderFactory.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level16.lesson13.bonus01;
+
+import com.javarush.test.level16.lesson13.bonus01.common.*;
+
+public class ImageReaderFactory {
+    public static ImageReader getReader(ImageTypes imageTypes){
+        if(imageTypes == ImageTypes.BMP){
+            return new BmpReader();
+        } else if(imageTypes == ImageTypes.JPG){
+            return new JpgReader();
+        } else if(imageTypes == ImageTypes.PNG){
+            return new PngReader();
+        } else {
+            throw new IllegalArgumentException("Неизвестный тип картинки");
+        }
+    }
+}
