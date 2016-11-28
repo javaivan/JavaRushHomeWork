f89e705003a23d6913831663a45812f760c56a4f
ivan <ivanmix_88@mail.ru>
2016-11-28 10:50:18 +0200

    package com.javarush.test.level18.lesson08.task02;

--- src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
@@ -13,5 +13,41 @@ import java.io.*;
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
 
+    private AmigoOutputStream amigoOutputStream;
+
+    public QuestionFileOutputStream(AmigoOutputStream amigoOutputStream) throws IOException {
+        this.amigoOutputStream = amigoOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        reader.close();
+        if(input.equals("Д")){
+            amigoOutputStream.close();
+        }
+    }
 }
 
