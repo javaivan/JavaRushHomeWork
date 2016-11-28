182a2edb646e67dc2adf0be593985e8555691aa6
ivan <ivanmix_88@mail.ru>
2016-11-28 11:09:58 +0200

    package com.javarush.test.level18.lesson08.task03;

--- src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
@@ -3,6 +3,7 @@ package com.javarush.test.level18.lesson08.task03;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 /* AmigoOutputStream
 1 Измените класс AmigoOutputStream так, чтобы он стал Wrapper-ом для класса FileOutputStream. Используйте наследование.
@@ -12,10 +13,44 @@ import java.io.IOException;
 2.3 закрыть поток методом close()
 */
 
-public class AmigoOutputStream {
+public class AmigoOutputStream extends FileOutputStream {
     public static String fileName = "C:/tmp/result.txt";
+    private FileOutputStream fileOutputStream;
+    private byte[] text = "JavaRush © 2012-2013 All rights reserved.".getBytes();
 
-    public static void main(String[] args) throws FileNotFoundException {
-        /*new AmigoOutputStream(new FileOutputStream(fileName));*/
+    public AmigoOutputStream(FileOutputStream file) throws FileNotFoundException {
+        super(fileName);
+        this.fileOutputStream = file;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.flush();
+        fileOutputStream.write(text);
+        fileOutputStream.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        AmigoOutputStream amigoOutputStream = new AmigoOutputStream(new FileOutputStream(fileName));
     }
 }
