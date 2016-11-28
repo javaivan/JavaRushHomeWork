6eb79c77fab4cd6535904f455e60f1a664be814a
ivan <ivanmix_88@mail.ru>
2016-11-28 10:38:48 +0200

    package com.javarush.test.level17.lesson10.bonus02;

--- src/com/javarush/test/level17/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level17/lesson10/bonus02/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.test.level17.lesson10.bonus02;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /* CRUD 2
 CrUD Batch - multiple Creation, Updates, Deletion
@@ -37,7 +40,84 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
-        //start here - начни тут
+    public static void main(String[] args) throws ParseException
+    {
+        int id = 1;
+        String operator = args[0];
+        if(operator.equals("-c")){
+            if(args.length >= (id+2)) {
+                addPerson(args, id);
+            }
+        }
+        if(operator.equals("-u")){
+            updatePerson(args, id);
+        }
+        if(operator.equals("-d")){
+            deletePerson(args);
+        }
+        if(operator.equals("-i")){
+            infoPerson(args);
+        }
     }
+
+    private synchronized static void addPerson(String[] args, int id) throws ParseException {
+        if(args.length >= (id+2)){
+            Person person = null;
+            String sex = args[id+1];
+            String name = args[id];
+            Date birthDay = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[id+2]);
+
+            if (sex.equals("м")) person = Person.createMale(name, birthDay);
+            if (sex.equals("ж")) person = Person.createFemale(name, birthDay);
+            allPeople.add(person);
+            System.out.println(allPeople.indexOf(person));
+            if(args.length > (id+5)){
+                addPerson(args,id+3);
+            }
+        }
+    }
+    private synchronized static void updatePerson(String[] args, int orderId) throws ParseException {
+        if(args.length >= (orderId+3)) {
+            Person person = null;
+            int id = Integer.parseInt(args[orderId]);
+            String sex = args[orderId+2];
+            String name = args[orderId+1];
+            Date birthDay = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[orderId+3]);
+            if (sex.equals("м")) person = Person.createMale(name, birthDay);
+            if (sex.equals("ж")) person = Person.createFemale(name, birthDay);
+            allPeople.set(id, person);
+            if(args.length >= (orderId+7)) {
+                updatePerson(args,orderId);
+            }
+        }
+    }
+    private synchronized static void deletePerson(String[] args){
+        for (int i = 1; i<args.length; i++){
+            int id = Integer.parseInt(args[i]);
+            if (id > allPeople.size() - 1) {
+                break;
+            }
+            Person person = allPeople.get(id);
+            person.setName(null);
+            person.setSex(null);
+            person.setBirthDay(null);
+            allPeople.set(id, person);
+        }
+    }
+
+    private synchronized static void infoPerson(String[] args){
+        for (int i = 1; i<args.length; i++){
+            int id = Integer.parseInt(args[i]);
+            if (id > allPeople.size() - 1) {
+                break;
+            }
+            Person person = allPeople.get(id);
+            String sex = null;
+            String birthDay = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH).format(person.getBirthDay());
+            if (person.getSex().equals(Sex.MALE))  sex = "м";
+            if (person.getSex().equals(Sex.FEMALE))  sex = "ж";
+            System.out.println(person.getName() + " " + sex + " " + birthDay);
+        }
+    }
+
 }
--- src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
@@ -13,34 +13,5 @@ import java.io.*;
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
 
-    @Override
-    public void flush() throws IOException
-    {
-
-    }
-
-    @Override
-    public void write(int b) throws IOException
-    {
-
-    }
-
-    @Override
-    public void write(byte[] b) throws IOException
-    {
-
-    }
-
-    @Override
-    public void write(byte[] b, int off, int len) throws IOException
-    {
-
-    }
-
-    @Override
-    public void close() throws IOException
-    {
-
-    }
 }
 
