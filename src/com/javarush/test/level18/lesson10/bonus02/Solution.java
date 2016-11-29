9d281c263dd2474f756d2b6c57677a5b56396cfb
ivan <ivanmix_88@mail.ru>
2016-11-29 12:43:54 +0200

    level18.lesson10.bonus02

--- src/com/javarush/test/level18/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus02/Solution.java
@@ -22,7 +22,48 @@ id productName price quantity
 19847983Куртка для сноубордистов, разм10173.991234
 */
 
+import java.io.*;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        String id = correctLength(getId(fileName), 8);
+        String productName = correctLength(args[1], 30);
+        String price = correctLength(args[2],8);
+        String quantity = correctLength(args[3], 4);
+
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true));
+        bufferedWriter.write(id + productName + price + quantity);
+        bufferedWriter.newLine();
+        bufferedWriter.close();
     }
+
+    private static String getId(String fileName) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+
+        long id = 0L;
+        String line;
+        while ((line=bufferedReader.readLine()) != null){
+            long i = Long.parseLong(line.substring(0,8).trim());
+            if(i > id){
+                id = i;
+            }
+        }
+
+        bufferedReader.close();
+        return id + 1 + "";
+    }
+
+    private static String correctLength(String s, int length){
+        if(s.length() < length){
+            return String.format("%1$-" + length + "s", s);
+        } else {
+            return s.substring(0,length);
+        }
+    }
+
 }
