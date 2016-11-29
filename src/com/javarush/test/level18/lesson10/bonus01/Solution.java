2f32daabc3dd24665cd3404018830883367df096
ivan <ivanmix_88@mail.ru>
2016-11-29 12:00:59 +0200

    level18.lesson10.bonus01

--- src/com/javarush/test/level18/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus01/Solution.java
@@ -1,4 +1,4 @@
-package com.javarush.test.level18.lesson10.bonus01;
+    package com.javarush.test.level18.lesson10.bonus01;
 
 /* Шифровка
 Придумать механизм шифровки/дешифровки
@@ -13,9 +13,32 @@ fileOutputName - имя файла, куда необходимо записат
 -d - ключ указывает, что необходимо расшифровать данные
 */
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String key = args[0];
+        String inFileName = args[1];
+        String outFileName = args[2];
 
+        FileInputStream fin = new FileInputStream(inFileName);
+        byte[] inFileBytes = new byte[fin.available()];
+        fin.read(inFileBytes);
+        fin.close();
+
+        if ("-e".equals(key)) {
+            for (int i = 0; i < inFileBytes.length; i++) {
+                inFileBytes[i] = (byte) (inFileBytes[i] + 1);
+            }
+        } else if ("-d".equals(key)) {
+            for (int i = 0; i < inFileBytes.length; i++) {
+                inFileBytes[i] = (byte) (inFileBytes[i] - 1);
+            }
+        }
+
+        FileOutputStream fout = new FileOutputStream(outFileName);
+        fout.write(inFileBytes);
+        fout.close();
     }
-
 }
