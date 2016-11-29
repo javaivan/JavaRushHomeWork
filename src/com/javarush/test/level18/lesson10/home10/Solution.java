f0058405f22d25b9ff4f086aff996a9b6c2ceccf
ivan <ivanmix_88@mail.ru>
2016-11-29 11:17:17 +0200

    level18.lesson10.home10;

--- src/com/javarush/test/level18/lesson10/home10/Solution.java
+++ src/com/javarush/test/level18/lesson10/home10/Solution.java
@@ -10,8 +10,34 @@ package com.javarush.test.level18.lesson10.home10;
 Файлы переписывать в строгой последовательности, сначала первую часть, потом вторую, ..., в конце - последнюю.
 Закрыть потоки. Не использовать try-with-resources
 */
+import java.io.*;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name;
+        Set<File> fileset = new TreeSet<>();
+        while (!(name = reader.readLine()).equals("end")) {
+            File file = new File(name);
+            fileset.add(file);
+        }
+        reader.close();
+        Iterator<File> itr = fileset.iterator();
+        String folder = itr.next().getAbsolutePath();
+        folder = folder.substring(0, folder.length() - 6);
+        FileOutputStream resultFile = new FileOutputStream(folder, true);
+        for (File file : fileset) {
+            FileInputStream in = new FileInputStream(file);
+            byte[] buffer = new byte[in.available()];
+            while (in.available() > 0) {
+                in.read(buffer);
+                resultFile.write(buffer);
+            }
+            in.close();
+        }
+        resultFile.close();
     }
 }
