13cc32747732c89b60fbc59080dc23da9caff65d
ivan <ivanmix_88@mail.ru>
2016-11-29 21:35:20 +0200

    level19.lesson03.task04

--- src/com/javarush/test/level19/lesson03/task04/Solution.java
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.test.level19.lesson03.task04;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 /* И еще один адаптер
@@ -13,7 +15,30 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static class PersonScannerAdapter {
+    public static class PersonScannerAdapter implements PersonScanner {
+        private Scanner scanner;
 
+        public PersonScannerAdapter(Scanner scanner){
+            this.scanner = scanner;
+        }
+
+        @Override
+        public Person read() throws IOException {
+            String line = scanner.nextLine();
+            String[] arr = line.split(" ");
+            String firstName = arr[1];
+            String middleName = arr[2];
+            String lastName = arr[0];
+            int year = Integer.parseInt(arr[5]);
+            int month = Integer.parseInt(arr[4]) - 1;
+            int day = Integer.parseInt(arr[3]);
+            Calendar calendar = new GregorianCalendar(year, month, day);
+            return new Person(firstName, middleName, lastName, calendar.getTime());
+        }
+
+        @Override
+        public void close() throws IOException {
+            scanner.close();
+        }
     }
 }
