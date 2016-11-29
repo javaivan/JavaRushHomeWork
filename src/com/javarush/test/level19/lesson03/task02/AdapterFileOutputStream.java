1751b5d36863cbcd4388ed5bd971d464afa471e3
ivan <ivanmix_88@mail.ru>
2016-11-29 18:38:20 +0200

    level19.lesson03.task02

--- src/com/javarush/test/level19/lesson03/task02/AdapterFileOutputStream.java
+++ src/com/javarush/test/level19/lesson03/task02/AdapterFileOutputStream.java
@@ -4,7 +4,29 @@ package com.javarush.test.level19.lesson03.task02;
 Используйте класс AdapterFileOutputStream, чтобы адаптировать FileOutputStream к новому интерфейсу AmigoStringWriter
 */
 
-public class AdapterFileOutputStream {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class AdapterFileOutputStream implements AmigoStringWriter{
+    private FileOutputStream fileOutputStream;
+
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream){
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        fileOutputStream.write(s.getBytes());
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.close();
+    }
 }
 
