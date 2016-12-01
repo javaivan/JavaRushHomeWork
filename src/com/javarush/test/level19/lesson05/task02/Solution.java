beb820e35aedf4f8670e7f18c60bf3528973ec5a
ivan <ivanmix_88@mail.ru>
2016-12-01 18:03:41 +0200

    level19.lesson05.task02

--- src/com/javarush/test/level19/lesson05/task02/Solution.java
+++ src/com/javarush/test/level19/lesson05/task02/Solution.java
@@ -7,7 +7,27 @@ package com.javarush.test.level19.lesson05.task02;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        fileReader.readLine();
+        String line;
+        int count = 0;
+        while ((line = fileReader.readLine()) != null){
+            line = line.replaceAll("[\\p{P}]", " ");
+            for (String s: line.split(" ")){
+                if(s.equals("world")){
+                    count++;
+                }
+            }
+        }
+        fileReader.close();
+        System.out.println(count);
     }
 }
