a42ab753c868f9f41cef275e22784e28d0ebb2df
ivan <ivanmix_88@mail.ru>
2016-12-01 17:51:40 +0200

    level19.lesson05.task01

--- src/com/javarush/test/level19/lesson05/task01/Solution.java
+++ src/com/javarush/test/level19/lesson05/task01/Solution.java
@@ -7,7 +7,26 @@ package com.javarush.test.level19.lesson05.task01;
 Закрыть потоки ввода-вывода.
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileOne = reader.readLine();
+        String fileTwo = reader.readLine();
+        reader.close();
+
+        FileInputStream inputStream = new FileInputStream(fileOne);
+        byte[] bytes = new byte[inputStream.available()];
+        inputStream.read(bytes);
+
+        FileOutputStream outputStream = new FileOutputStream(fileTwo);
+        for (int i = 0; i < bytes.length; i++) {
+            if (i % 2 == 1) {
+                outputStream.write(bytes[i]);
+            }
+        }
+        inputStream.close();
+        outputStream.close();
     }
 }
