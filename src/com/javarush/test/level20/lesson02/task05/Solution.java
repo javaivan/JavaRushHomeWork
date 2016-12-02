1f74455492af100ba32a8a50a21ffd0eb118e761
ivan <ivanmix_88@mail.ru>
2016-12-02 20:26:16 +0200

    level20.lesson02.task05

--- src/com/javarush/test/level20/lesson02/task05/Solution.java
+++ src/com/javarush/test/level20/lesson02/task05/Solution.java
@@ -9,17 +9,20 @@ import java.io.*;
 Метод main реализован только для вас и не участвует в тестировании
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(java.lang.String[] args) {
         //you can find your_file_name.tmp in your TMP directory or fix outputStream/inputStream according to your real file location
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
-            File your_file_name = File.createTempFile("your_file_name", null);
+            File your_file_name = new File("src/main/resources/level20/lesson02/task05/t1.txt");
             OutputStream outputStream = new FileOutputStream(your_file_name);
             InputStream inputStream = new FileInputStream(your_file_name);
 
             Object object = new Object();
             object.string1 = new String();   //string #1
             object.string2 = new String();   //string #2
+
+            new String();
+
             object.save(outputStream);
             outputStream.flush();
 
@@ -27,18 +30,20 @@ public class Solution {
             loadedObject.string1 = new String(); //string #3
             loadedObject.string2 = new String(); //string #4
 
-
             loadedObject.load(inputStream);
             //check here that the object variable equals to loadedObject - проверьте тут, что object и loadedObject равны
+
+            loadedObject.string1.print();
+            loadedObject.string2.print();
 
             outputStream.close();
             inputStream.close();
 
         } catch (IOException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
             System.out.println("Oops, something wrong with my file");
         } catch (Exception e) {
-            //e.printStackTrace();
+            e.printStackTrace();
             System.out.println("Oops, something wrong with save/load method");
         }
     }
@@ -50,10 +55,25 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            PrintWriter fWriter = new PrintWriter(outputStream);
+            fWriter.println(string1.number + "");
+            fWriter.println(string2.number + "");
+            fWriter.close();
         }
 
         public void load(InputStream inputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            BufferedReader fReader = new BufferedReader(new InputStreamReader(inputStream));
+            int tmp = countStrings;
+
+            countStrings = Integer.parseInt(fReader.readLine()) - 1;
+            this.string1 = new String();
+
+            countStrings = Integer.parseInt(fReader.readLine()) - 1;
+            this.string2 = new String();
+
+            fReader.close();
+            countStrings = tmp;
         }
     }
 
