b7afdf41e0f4839e67f8dc8da438f9c193644fe6
ivan <ivanmix_88@mail.ru>
2016-12-02 19:23:24 +0200

    level20.lesson02.task03

--- src/com/javarush/test/level20/lesson02/task03/Solution.java
+++ src/com/javarush/test/level20/lesson02/task03/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.test.level20.lesson02.task03;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /* Знакомство с properties
 В методе fillInPropertiesMap считайте имя файла с консоли и заполните карту properties данными из файла.
@@ -10,15 +13,34 @@ import java.io.*;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+    public void fillInPropertiesMap() throws IOException
+    {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = consoleReader.readLine();
+        consoleReader.close();
+        try {
+            load(new FileInputStream(fileName));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         //implement this method - реализуйте этот метод
     }
 
     public void save(OutputStream outputStream) throws Exception {
+        Properties prop = new Properties();
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            prop.put(entry.getKey(), entry.getValue());
+        }
+        prop.store(outputStream,"");
         //implement this method - реализуйте этот метод
     }
 
     public void load(InputStream inputStream) throws Exception {
+        Properties prop = new Properties();
+        prop.load(inputStream);
+        for (Map.Entry<Object,Object> entry: prop.entrySet()){
+            properties.put(entry.getKey().toString(), entry.getValue().toString());
+        }
         //implement this method - реализуйте этот метод
     }
 }
