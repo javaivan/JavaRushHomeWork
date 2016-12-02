80096d52922622372d73e574fc8f837e0e087c69
ivan <ivanmix_88@mail.ru>
2016-12-02 09:58:58 +0200

    level19.lesson05.task05

--- src/com/javarush/test/level19/lesson05/task05/Solution.java
+++ src/com/javarush/test/level19/lesson05/task05/Solution.java
@@ -8,7 +8,22 @@ http://ru.wikipedia.org/wiki/%D0%9F%D1%83%D0%BD%D0%BA%D1%82%D1%83%D0%B0%D1%86%D0
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileOne = consoleReader.readLine();
+        String fileTwo = consoleReader.readLine();
+        consoleReader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileOne));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileTwo));
+        String line;
+        while ((line = fileReader.readLine()) != null){
+            fileWriter.write(line.replaceAll("(?U)[\\pP\\s]", ""));
+        }
+        fileReader.close();
+        fileWriter.close();
     }
 }
