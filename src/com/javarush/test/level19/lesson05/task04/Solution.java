6e956b4c9280a3c172221d38ab489576812a7345
ivan <ivanmix_88@mail.ru>
2016-12-02 09:49:55 +0200

    level19.lesson05.task04

--- src/com/javarush/test/level19/lesson05/task04/Solution.java
+++ src/com/javarush/test/level19/lesson05/task04/Solution.java
@@ -7,7 +7,29 @@ package com.javarush.test.level19.lesson05.task04;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        String fileOne = consoleReader.readLine();
+        String fileTwo = consoleReader.readLine();
+        consoleReader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileOne));
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        while ((line = fileReader.readLine()) != null){
+            lines.add(line.replace(".","!"));
+        }
+        fileReader.close();
+
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileTwo));
+        for (String s: lines){
+            fileWriter.write(s);
+            fileWriter.newLine();
+        }
+        fileWriter.close();
     }
 }
