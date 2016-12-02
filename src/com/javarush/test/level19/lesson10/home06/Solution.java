0c672f786a5f0f4f34bdb10b2ddcb987ccc5fa31
ivan <ivanmix_88@mail.ru>
2016-12-02 14:11:36 +0200

    level19.lesson10.home06

--- src/com/javarush/test/level19/lesson10/home06/Solution.java
+++ src/com/javarush/test/level19/lesson10/home06/Solution.java
@@ -1,7 +1,12 @@
 package com.javarush.test.level19.lesson10.home06;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /* Замена чисел
 1. В статическом блоке инициализировать словарь map парами [число-слово] от 0 до 12 включительно
@@ -25,7 +30,42 @@ import java.util.Map;
 public class Solution {
     public static Map<Integer, String> map = new HashMap<Integer, String>();
 
-    public static void main(String[] args) {
+    static {
+        map.put(0, "ноль");
+        map.put(1, "один");
+        map.put(2, "два");
+        map.put(3, "три");
+        map.put(4, "четыре");
+        map.put(5, "пять");
+        map.put(6, "шесть");
+        map.put(7, "семь");
+        map.put(8, "восемь");
+        map.put(9, "девять");
+        map.put(10, "десять");
+        map.put(11, "одиннадцать");
+        map.put(12, "двенадцать");
+    }
 
+    public static void main(String[] args) throws IOException {
+        Pattern pattern = Pattern.compile("[0-9]|10|11|12");
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = consoleReader.readLine();
+        consoleReader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = fileReader.readLine()) != null){
+            String[] split = line.split(" ");
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String s: split){
+                if(pattern.matcher(s).matches()){
+                    stringBuilder.append(map.get(Integer.parseInt(s))).append(" ");
+                } else {
+                    stringBuilder.append(s).append(" ");
+                }
+            }
+            System.out.println(stringBuilder.toString().trim());
+        }
+        fileReader.close();
     }
 }
