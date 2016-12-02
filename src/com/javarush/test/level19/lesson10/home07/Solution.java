8da403acc034ce2c0c97a88044581bc1438bd81b
ivan <ivanmix_88@mail.ru>
2016-12-02 14:40:33 +0200

    level19.lesson10.home07

--- src/com/javarush/test/level19/lesson10/home07/Solution.java
+++ src/com/javarush/test/level19/lesson10/home07/Solution.java
@@ -10,8 +10,28 @@ package com.javarush.test.level19.lesson10.home07;
 длинное,короткое,аббревиатура
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String fileOneName = args[0];
+        String fileTwoName = args[1];
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileOneName));
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = fileReader.readLine()) != null){
+            String[] split = line.split(" ");
+            for (String s: split){
+                if(s.length()>6){
+                    stringBuilder.append(s).append(",");
+                }
+            }
+        }
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTwoName));
+        stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "");
+        bufferedWriter.write(stringBuilder.toString());
+        fileReader.close();
+        bufferedWriter.close();
     }
 }
