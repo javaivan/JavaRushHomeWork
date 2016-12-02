9a09cf2941ef06bb33ed2e2bf46cf47f600e8e0e
ivan <ivanmix_88@mail.ru>
2016-12-02 09:42:18 +0200

    level19.lesson05.task03

--- src/com/javarush/test/level19/lesson05/task03/Solution.java
+++ src/com/javarush/test/level19/lesson05/task03/Solution.java
@@ -13,7 +13,36 @@ package com.javarush.test.level19.lesson05.task03;
 12 14 1
 */
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileOne = consoleReader.readLine();
+        String fileTwo = consoleReader.readLine();
+        consoleReader.close();
+
+        BufferedReader fileOneReader = new BufferedReader(new FileReader(fileOne));
+
+        ArrayList<Integer> list = new ArrayList<>();
+        String line;
+        while ((line = fileOneReader.readLine()) != null){
+            for (String s: line.split(" ")){
+                try {
+                    Integer i = Integer.parseInt(s);
+                    list.add(i);
+                } catch (NumberFormatException e){
+                    //ok}
+                }
+            }
+        }
+        BufferedWriter fileTwoWriter = new BufferedWriter(new FileWriter(fileTwo));
+        for (Integer i: list){
+            fileTwoWriter.write(i + " ");
+        }
+        fileOneReader.close();
+        fileTwoWriter.close();
+
     }
 }
