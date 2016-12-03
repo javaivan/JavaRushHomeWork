5ab3b3d84c1fc096df9eedb651cc360306b22b33
ivan <ivanmix_88@mail.ru>
2016-12-03 11:09:34 +0200

    level20.lesson07.task01

--- src/com/javarush/test/level20/lesson07/task01/Solution.java
+++ src/com/javarush/test/level20/lesson07/task01/Solution.java
@@ -1,12 +1,17 @@
 package com.javarush.test.level20.lesson07.task01;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 /* Externalizable для апартаментов
 Реализуйте интерфейс Externalizable для класса Apartment
 Подумайте, какие поля не нужно сериализовать.
 */
 public class Solution {
 
-    public static class Apartment {
+    public static class Apartment implements Externalizable {
 
         private String address;
         private int year;
@@ -26,6 +31,18 @@ public class Solution {
          */
         public String toString() {
             return("Address: " + address + "\n" + "Year: " + year);
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(address);
+            out.writeInt(year);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            address = (String) in.readObject();
+            year = in.readInt();
         }
     }
 }
