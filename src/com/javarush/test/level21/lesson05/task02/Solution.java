1cb9648b1de89175aa90adaca0742ba92aa632fe
ivan <ivanmix_88@mail.ru>
2016-12-04 16:20:37 +0200

    level21.lesson05 task01 - task03

--- src/com/javarush/test/level21/lesson05/task02/Solution.java
+++ src/com/javarush/test/level21/lesson05/task02/Solution.java
@@ -15,11 +15,22 @@ public class Solution {
         this.last = last;
     }
 
+    @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Solution))
-            return false;
-        Solution n = (Solution) o;
-        return n.first.equals(first) && n.last.equals(last);
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        return last != null ? last.equals(solution.last) : solution.last == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
     }
 
     public static void main(String[] args) {
