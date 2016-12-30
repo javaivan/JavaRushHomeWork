cc056f2165d559ef81f46d038093085d948d9391
Ivan <ivanmix_88@mail.ru>
2016-12-29 21:45:49 -0800

    init

---
+++ src/com/javarush/test/level31/lesson02/home02/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level31.lesson02.home02;
+
+import java.io.IOException;
+import java.util.List;
+
+/* Находим все файлы
+Реализовать логику метода getFileTree, который должен в директории root найти список всех файлов включая вложенные.
+Используйте очередь, рекурсию не используйте.
+Верните список всех путей к найденным файлам, путь к директориям возвращать не надо.
+Путь должен быть абсолютный.
+*/
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        return null;
+
+    }
+}
